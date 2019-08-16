8f336f85edd38378a892c152c1f39dff90fc8545
ardeshirj <ardeshir.javaherchi@gmail.com>
2019-08-15 20:26:21 -0700

    Add invoice module

---
+++ src/invoice/components/invoice-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'invoice-table',
+  templateUrl: './invoice-table.component.html',
+  styleUrls: ['./invoice-table.component.scss']
+})
+export class InvoiceTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
