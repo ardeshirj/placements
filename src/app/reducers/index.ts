2e9b65b5f00a0af4e95353fa254b241f1c640a53
ardeshirj <ardeshir.javaherchi@gmail.com>
2019-08-15 21:52:45 -0700

    Add router reducer

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -5,15 +5,16 @@ import {
   createSelector,
   MetaReducer
 } from '@ngrx/store';
+import * as fromRouter from '@ngrx/router-store';
+
 import { environment } from '../../environments/environment';
 
 export interface State {
-
+  router: fromRouter.RouterReducerState<any>;
 }
 
 export const reducers: ActionReducerMap<State> = {
-
+  router: fromRouter.routerReducer,
 };
-
 
 export const metaReducers: MetaReducer<State>[] = !environment.production ? [] : [];
